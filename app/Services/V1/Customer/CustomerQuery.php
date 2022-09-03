1cd688203503e5e4819d2b8dbd0e2fe3ae5af013
tutdeputraw <anoboy.code@gmail.com>
2022-09-03 21:43:08 +0700

    filtering data

---
+++ app/Services/V1/Customer/CustomerQuery.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services\V1\Customer;
+
+use Illuminate\Http\Request;
+
+class CustomerQuery
+{
+    protected $safeParams = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['eq', 'gt', 'lt']
+    ];
+
+    protected $columnMap = [
+        'postalCode' => 'postal_code'
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+    ];
+
+    public function transform(Request $request)
+    {
+        $eloQuery = [];
+
+        foreach ($this->safeParams as $param => $operators) {
+            $query = $request->query($param); //query email, address, etc
+
+            if (!isset($query)) {
+                continue;
+            }
+
+            $column = $this->columnMap[$param] ?? $param;
+
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+
+        return $eloQuery;
+    }
+}
