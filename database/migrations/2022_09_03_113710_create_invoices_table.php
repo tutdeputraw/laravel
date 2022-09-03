a3aca44c0f4cea7718b72fda2940bd099096b4a4
tutdeputraw <anoboy.code@gmail.com>
2022-09-03 19:12:50 +0700

    designing and seeding the database

--- database/migrations/2022_09_03_113710_create_invoices_table.php
+++ database/migrations/2022_09_03_113710_create_invoices_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->integer('customer_id');
+            $table->integer('amount');
+            $table->string('status'); //billed, paid, void
+            $table->dateTime('billed_date');
+            $table->dateTime('paid_date')->nullable();
             $table->timestamps();
         });
     }
